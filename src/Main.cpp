435251f81dbb826b4b60163609c02725a750d03c
Adwait Rawat <adwait.rawat@gmail.com>
2019-06-17 11:26:25 +0900

    basic file testing

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath>
 #include <cstdlib>
 #include <cstdio>
 #include "Body.hpp"
@@ -7,9 +6,61 @@
 #include "Graphics.hpp"
 #include "Math.hpp"
 
+using std::cout;
+using std::endl;
+
 int main (int argc, char **argv) {
-	std::cout<<argc<<std::endl;
-	std::cout<<argv[0]<<std::endl;
+	cout << argc << endl;
+	cout << argv[0] << endl;
+
+	Vector3d x, y;
+	x << 1, 0, 0;
+	y << 0, 1, 0;
+
+	Celestia::Math math;
+	std::vector <Celestia::Body> body;
+	Celestia::Constants constant;
+
+	Vector3d xunit = math.unit (x);
+	Vector3d yunit = math.unit (y);
+
+ 	std::vector <double> input1 = {1, 1, 1, 0, 0, 0, 0, 0};
+	std::vector <double> input2 = {1, 1, -1, 0, 0, 0, 0, 0};
+
+	body.push_back(Celestia::Body());
+	body[0].create (input1);
+	body.push_back(Celestia::Body());
+	body[1].create (input2);
+
+	cout << "Testing Math" << endl;
+	cout << "Vector x:\n" << x << endl;
+	cout << "Vector y:\n" << y << endl;
+	cout << "Mag of x: " << math.magnitude (x) << endl;
+	cout << "Mag of y: " << math.magnitude (y) << endl;
+	cout << "X Unit vector:\n" << xunit << endl;
+	cout << "Y Unit vector:\n" << yunit << endl;
+	cout << "Mag of X unit: " << math.magnitude (xunit) << endl;
+	cout << "Mag of Y unit: " << math.magnitude (yunit) << endl;
+	cout << "Distance between x y: " << math.distance (x, y) << endl;
+	cout << "Dot product: " << math.dot (x, y) << endl;
+	cout << "Cross product:\n" << math.cross (x, y) << endl;
+	cout << "X -> Y:\n" << math.delta (x, y) << endl;
+	cout << "Y -> X:\n" << math.delta (y, x) << endl;
+
+	cout << "~~~~~~~~~~~~~~~~" << endl;
+
+	cout << "Testing Constants" << endl;
+	cout << "Constant G = " << constant.G << endl;
+	cout << "Constant e = " << constant.e << endl;
+
+	cout << "~~~~~~~~~~~~~~~~" << endl;
+
+	cout << "Testing Body" << endl;
+	for (int i = 0; i < 2; i++) {
+		cout << "Body " << i <<" to string: " << body[i].to_string() << endl;
+		cout << "Body " << i <<" Acceleration:\n" << body[i].acceleration << endl;
+	}
+
 	return 0;
 }
 
