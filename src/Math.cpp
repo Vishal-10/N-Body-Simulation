357f7d7a347256c13ac47fbc6ea14415b881a63b
Adwait Rawat <adwait.rawat@gmail.com>
2019-06-17 11:24:23 +0900

    Math: Funtion additions and fixing

--- src/Math.cpp
+++ src/Math.cpp
@@ -2,13 +2,22 @@
 
 Celestia::Math::Math () {}
 
+Vector3d Celestia::Math::delta (Vector3d vec1, Vector3d vec2) {
+	return vec2 - vec1;
+}
+
 double Celestia::Math::distance (Vector3d vec1, Vector3d vec2) {
+	Vector3d difference = vec1 - vec2;
+	return sqrt (difference.dot (difference));
+}
+
+double Celestia::Math::sq_distance (Vector3d vec1, Vector3d vec2) {
 	Vector3d difference = vec1 - vec2;
 	return difference.dot (difference);
 }
 
 double Celestia::Math::magnitude (Vector3d vec) {
-	return vec.dot (vec);
+	return sqrt (vec.dot (vec));
 }
 
 Vector3d Celestia::Math::cross (Vector3d vec1, Vector3d vec2) {
@@ -17,4 +26,12 @@ Vector3d Celestia::Math::cross (Vector3d vec1, Vector3d vec2) {
 
 double Celestia::Math::dot (Vector3d vec1, Vector3d vec2) {
 	return vec1.dot (vec2);
+}
+
+Vector3d Celestia::Math::scalar (double scalar, Vector3d vec) {
+	return scalar*vec;
+}
+
+Vector3d Celestia::Math::unit (Vector3d vec) {
+	return vec/(Celestia::Math::magnitude (vec));
 }
--- src/Math.hpp
+++ src/Math.hpp
@@ -3,7 +3,7 @@
 
 #include <cmath>
 #include <vector>
-#include "Eigen/Core"
+#include "Eigen/Dense"
 #include "Body.hpp"
 
 using Eigen::Vector3d;
@@ -13,10 +13,14 @@ namespace Celestia {
 	public:
 		Math ();
 
+		Vector3d delta (Vector3d vec1, Vector3d vec2);
 		double distance (Vector3d vec1, Vector3d vec2);
+		double sq_distance (Vector3d vec1, Vector3d vec2);
 		double magnitude (Vector3d vec);
 		Vector3d cross (Vector3d vec1, Vector3d vec2);
 		double dot (Vector3d vec1, Vector3d vec2);
+		Vector3d scalar (double scalar, Vector3d vec);
+		Vector3d unit (Vector3d);
 	};
 }
 
