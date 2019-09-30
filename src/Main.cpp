671a35f1a391086ed28993d6cd64895db95b4cc6
Gaurav Agrawal <agrawalgaurav1999@gmail.com>
2019-09-30 21:37:01 +0530

    universal set for testing algorithm

--- src/Main.cpp
+++ src/Main.cpp
@@ -17,19 +17,19 @@ int main (int argc, char **argv) {
 	Celestia::Math math;
 	std::vector <Celestia::Body> bodies;
 
- 	std::vector <double> input1 = {1, 0, 0, 0, 0.5, 0};
-	std::vector <double> input2 = {-1, 0, 0, 0, -0.5, 0};
+	std::vector <double> input1 = {0.5, 0, 0, 0, 0.5, 0};
+	std::vector <double> input2 = {-0.5, 0, 0, 0, -0.5, 0};
 
 	bodies.push_back(Celestia::Body());
 	bodies[0].default_create (input1);
 	bodies.push_back(Celestia::Body());
 	bodies[1].default_create (input2);
 
-	Celestia::Simulator sim (bodies, 10, 0.001);
+	Celestia::Simulator sim (argc, argv);
+	sim.simulate (bodies, 10, 0.0001);
 	cout << "Testing Simulator: Run begins" << endl;
 	sim.run ();
 	cout << "Run End" << endl;
 
 	return 0;
 }
-
